dfd574417c4f0ba9d559526a153aeaa752a559dd
shiphitchcock1 <shiphitchcock1@gmail.com>
2017-05-23 11:28:53 +0530

    Update echo.sh

--- echo.sh
+++ echo.sh
@@ -1,18 +1,22 @@
 #!/bin/bash -e
 echo 'this is our runsh'
-if [ "$FOO" == "CE1" ]; then 
-    echo "Test pass if FOO=CE1 else test is fail";
-    exit 1; 
-#else
- #   return 0;
+if [[ "$BOO" == "CE2" ]]; then
+  MY_SCRIPT_VARIABLE="Some default value because DEPLOY_ENV is undefined"
+else
+  MY_SCRIPT_VARIABLE="$BOO"
+fi;
 
+echo ${MY_SCRIPT_VARIABLE}
 #if [ "$BOO" == "CE2" ]; then 
 #    echo "Test pass if FOO=CE2 else test is fail"; 
- #   exit 1; 
-#else
-  #  return 0;
-
+ #   exit 1;
 
 #echo 'EOF' 
 #echo $FOO
 #echo $BOO
+
+#if [[ -z "${DEPLOY_ENV}" ]]; then
+#  MY_SCRIPT_VARIABLE="Some default value because DEPLOY_ENV is undefined"
+#else
+#  MY_SCRIPT_VARIABLE="${DEPLOY_ENV}"
+#fi
