6004d5f78408fe5bef9280013e11eb1ee4cca4c4
shiphitchcock1 <shiphitchcock1@gmail.com>
2017-05-23 11:20:48 +0530

    Update echo.sh

--- echo.sh
+++ echo.sh
@@ -2,9 +2,9 @@
 echo 'this is our runsh'
 if [ "$FOO" == "CE1" ]; then 
     echo "Test pass if FOO=CE1 else test is fail"; 
-    exit 1; 
-    else
-      return 0;
+    return 1; 
+else
+    return 0;
 
 #if [ "$BOO" == "CE2" ]; then 
 #    echo "Test pass if FOO=CE2 else test is fail"; 
