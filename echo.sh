0b10c83e88cc0d267bcf254d3c369c3e91b1cceb
shiphitchcock1 <shiphitchcock1@gmail.com>
2017-05-23 16:33:00 +0530

    Update echo.sh

--- echo.sh
+++ echo.sh
@@ -1,76 +1,85 @@
 #!/bin/bash -e
 
-echo 'this is our runsh'
+echo 'Environment variable section'
+echo $FOO
+echo $BOO
+echo $MATRIXINCLUDE1
+echo $MATRIXINCLUDE2
+echo $MATRIXINCLUDE3
+echo $a
+echo $b
+echo $c
+echo $d
+echo $ALPHA
 
 if [[ "$FOO" == "CE1" ]]; then
-  echo "BOO=CE1 set from custom build"
+  echo "FOO=$FOO set from custom build"
 else
-  echo ERROR: Failed to see $FOO. Please recheck the env variables are coming from custom builds
+  echo ERROR: Failed, wrong value forthe FOO variable. Please recheck the env variables are coming from custom builds
   exit 1
 fi;
 
 if [[ "$BOO" == "CE2" ]]; then
-  echo "BOO=CE2 set from custom build"
+  echo "BOO=$BOO set from custom build"
 else
-  echo ERROR: Failed to see $BOO. Please recheck the env variables are coming from custom builds
+  echo ERROR: Failed, wrong value forthe BOO variable. Please recheck the env variables are coming from custom builds
   exit 1
 fi;
 
 if [[ "$MATRIXINCLUDE1" == "CE3" ]]; then
-  echo "MATRIXINCLUDE1=CE3 set from custom build"
+  echo "MATRIXINCLUDE1=$MATRIXINCLUDE1 set from custom build"
 else
-  echo ERROR: Failed to see $MATRIXINCLUDE1. Please recheck the env variables are coming from custom builds
+  echo ERROR: Failed, wrong value forthe MATRIXINCLUDE1 variable. Please recheck the env variables are coming from custom builds
   exit 1
 fi;
 
 if [[ "$MATRIXINCLUDE2" == "CE4" ]]; then
-  echo "MATRIXINCLUDE2=CE4 set from custom build"
+  echo "MATRIXINCLUDE2=$MATRIXINCLUDE2 set from custom build"
 else
-  echo ERROR: Failed to see $MATRIXINCLUDE2. Please recheck the env variables are coming from custom builds
+  echo ERROR: Failed, wrong value forthe MATRIXINCLUDE2 variable. Please recheck the env variables are coming from custom builds
   exit 1
 fi;
 
 if [[ "$MATRIXINCLUDE3" == "CE5" ]]; then
-  echo "MATRIXINCLUDE3=CE5 set from custom build"
+  echo "MATRIXINCLUDE3=$MATRIXINCLUDE3 set from custom build"
 else
-  echo ERROR: Failed to see $MATRIXINCLUDE3. Please recheck the env variables are coming from custom builds
+  echo ERROR: Failed, wrong value forthe MATRIXINCLUDE3 variable. Please recheck the env variables are coming from custom builds
   exit 1
 fi;
 
-if [[ "$a" == "CE6" ]]; then
-  echo "a=CE6 set from custom build"
+if [[ "$A" == "CE6" ]]; then
+  echo "A=$A set from custom build"
 else
-  echo ERROR: Failed to see $a. Please recheck the env variables are coming from custom builds
+  echo ERROR: Failed, wrong value forthe A variable. Please recheck the env variables are coming from custom builds
   exit 1
 fi;
 
-if [[ "$b" == "CE7" ]]; then
-  echo "b=CE7 set from custom build"
+if [[ "$B" == "CE7" ]]; then
+  echo "B=$B set from custom build"
 else
-  echo ERROR: Failed to see $a. Please recheck the env variables are coming from custom builds
+  echo ERROR: Failed, wrong value forthe B variable. Please recheck the env variables are coming from custom builds
   exit 1
 fi;
 
-if [[ "$c" == "CE8" ]]; then
-  echo "c=CE8 set from custom build"
+if [[ "$C" == "CE8" ]]; then
+  echo "C=$C set from custom build"
 else
-  echo ERROR: Failed to see $c. Please recheck the env variables are coming from custom builds
+  echo ERROR: Failed, wrong value forthe C variable. Please recheck the env variables are coming from custom builds
   exit 1
 fi;
 
-if [[ "$d" == "CE9" ]]; then
-  echo "d=CE9 set from custom build"
+if [[ "$D" == "CE9" ]]; then
+  echo "D=$D set from custom build"
 else
-  echo ERROR: Failed to see $d. Please recheck the env variables are coming from custom builds
+  echo ERROR: Failed, wrong value forthe D variable. Please recheck the env variables are coming from custom builds
+  exit 1
+fi;
+
+if [[ "$ALPHA" == "CE10secure" ]]; then
+  echo "ALPHA=$ALPHA set from custom build"
+else
+  echo ERROR: Failed, wrong value forthe ALPHA variable. Please recheck the env variables are coming from custom builds
   exit 1
 fi;
 
 echo 'EOF' 
-echo $FOO
-echo $BOO
-
-#if [[ -z "${DEPLOY_ENV}" ]]; then
-#  MY_SCRIPT_VARIABLE="Some default value because DEPLOY_ENV is undefined"
-#else
-#  MY_SCRIPT_VARIABLE="${DEPLOY_ENV}"
-#fi
